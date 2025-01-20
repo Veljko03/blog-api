aa4c3d31e047968eb2ca275285696acbd83a4d80
Veljko03 <veljkodovragovic@gmail.com>
2025-01-20 22:00:07 +0100

    added cors

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const { post, signIn, logIn } = require("./routes");
 const app = express();
 const passport = require("passport");
 require("./config/passport");
-
+const cors = require("cors");
+app.use(cors());
 app.use(express.json());
 app.use("/posts", post);
 app.use("/log-in", logIn);
